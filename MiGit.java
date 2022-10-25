fbba78b31d974ef294cca6a1b93a79575b351eb3
HP196\CES <informatica@escuelaces.com>
2022-10-25 13:00:24 +0200

    prueba

--- MiGit.java
+++ MiGit.java
@@ -1,5 +1,6 @@
 public class MiGit {
 	public static void main(String[] args) {
-	System.out.println("Hola Mundo"); 
+		System.out.println("Hola Mundo"); 
+		System.out.println("¿Qué Tal?"); 
 	}
 }
